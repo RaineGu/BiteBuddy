69d5c154741daabcde6faa0e80c91720c47174a4
RaineGu <raineleigu@gmail.com>
2023-08-26 16:09:20 +1000

    ADD: Setup for flask and template files

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Hello, World!"
+    greeting = "Hello there, Ace"
+    return render_template("index.html", greet=greeting)
+
+
+app.run(debug=True)
