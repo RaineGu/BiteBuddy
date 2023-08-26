550012c39cf8b88d0041dff4af89b91194234c9c
RaineGu <raineleigu@gmail.com>
2023-08-26 16:17:26 +1000

    ADD: Function stubs for all functions in app.py

--- app.py
+++ app.py
@@ -9,4 +9,28 @@ def index():
     return render_template("index.html", greet=greeting)
 
 
+@app.get("/map")
+def map():
+    # Map shenanigans here
+    return render_template("map.html")
+
+
+@app.post("/map_filter")
+def map():
+    # Map filtering shenanigans here
+    return render_template("map.html")
+
+
+@app.get("/about_me")
+def map():
+    # About me shenanigans here
+    return render_template("about_me.html")
+
+
+@app.get("/recipes")
+def map():
+    # Recipe shenanigans here
+    return render_template("recipes.html")
+
+
 app.run(debug=True)
